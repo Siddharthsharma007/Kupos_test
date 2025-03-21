a0145d10f685220e31747d4d33fc5681f18e66e5
Siddharthsharma007 <56622300+Siddharthsharma007@users.noreply.github.com>
2025-03-21 15:30:54 +0530

    some issue fixes with seat selection

--- src/test/java/searchFlow.java
+++ src/test/java/searchFlow.java
@@ -1,8 +1,6 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import lombok.SneakyThrows;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
@@ -13,6 +11,7 @@ import java.util.List;
 
 public class searchFlow {
 
+    @SneakyThrows
     public static void main(String[] args) {
             WebDriverManager.chromedriver().setup();
             WebDriver driver = new ChromeDriver();
@@ -24,41 +23,30 @@ public class searchFlow {
             driver.findElement(By.xpath("(//*[@class='kupos-button_kupos_button__MM3z5 kupos-button'])[1]")).click();
             driver.findElement(By.xpath("//*[@class='kupos-button2']")).click();
 
-          WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-           WebElement dropdown = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//*[@class='common-kupos-select-box-outer'])[3]")));
-           dropdown.click();
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+            WebElement dropdown = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//*[@class='common-kupos-select-box-outer'])[3]")));
+            dropdown.click();
 
-          List<WebElement> options = dropdown.findElements(By.xpath("//div[@class='auto-suggestions font10 scroll-style ']//a"));
-        for (WebElement option : options) {
-            if (option.getText().equals("Plazuela Buin - 00:15")) {
+            List<WebElement> options = dropdown.findElements(By.xpath("//div[@class='auto-suggestions font10 scroll-style ']//a"));
+            for (WebElement option : options) {
+            if (option.getText().contains("Plazuela Buin")) {
                 option.click();
                 break;
             }
         }
 
-        List<WebElement> optionsnew = dropdown.findElements(By.xpath("//div[@class='index_coach_table__zXLYy coach-table light-text']//ul/li"));
+            List<WebElement> optionsnew = dropdown.findElements(By.xpath("//div[@class='index_coach_table__zXLYy coach-table light-text']//ul/li"));
+            System.out.println("Total options: " + optionsnew.size());
+            optionsnew.get(13).click();
+            WebDriverWait waitn = new WebDriverWait(driver, Duration.ofSeconds(10));
+            By buttonLocator = By.xpath("(//*[@class='kupos-button_kupos_button__MM3z5 kupos-button'])[2]");
 
-        System.out.println("Total options: " + optionsnew.size());
+        // Wait for the button to be clickable
+           WebElement button = waitn.until(ExpectedConditions.elementToBeClickable(buttonLocator));
 
-        // Find already selected element (if any)
-        WebElement selectedElement = null;
-        for (WebElement option : optionsnew) {
-            if (option.getAttribute("class").contains("selected")) { // Adjust based on actual class name
-                selectedElement = option;
-                break;
-            }
-        }
 
-        // Find an unselected element
-        for (WebElement option : optionsnew) {
-            if (selectedElement == null || !option.equals(selectedElement)) {
-                System.out.println("Clicking on: " + option.getText());
-                option.click();
-                break;
-            }
-        }
-
-        driver.findElement(By.xpath("(//*[@class='kupos-button_kupos_button__MM3z5 kupos-button'])[2]")).click();
+// Click using JavaScript
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", button);
 
 
     }
